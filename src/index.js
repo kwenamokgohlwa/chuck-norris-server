deea7063e91d438470cc161bc6e761b46e527950
kwenamokgohlwa <kwena@xago.io>
2019-08-09 20:28:04 +0200

    Major structural and code changes

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require('apollo-server');
+const fetch = require("node-fetch");
+
+const baseURL = `https://api.chucknorris.io/jokes`
+
+const typeDefs = gql`
+  type Query {
+    categories: [String!]!
+    joke(category: String!): Joke!
+  }
+  type Joke {
+    id: String!
+    url: String!
+    created_at: String!
+    updated_at: String!
+    icon_url: String!
+    categories: [String!]
+    value: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    categories: () => {
+      return fetch(`${baseURL}/categories`).then(res => res.json())
+    },
+    joke: (parent, args) => {
+      const { category } = args
+      return fetch(`${baseURL}/random?category=${category}`).then(res => res.json())
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
